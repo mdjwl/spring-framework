0bd2052db756d222e71bf808e8e7293b623d00c1
wanglu11413 <wanglu11413@autohome.com.cn>
2019-08-02 16:03:29 +0800

    aop

---
+++ spring-study/src/main/java/com/wanglu/cn/UserBeanDefinitionParser.java
@@ -0,0 +1,6 @@
+package com.wanglu.cn;
+
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+
+public class UserBeanDefinitionParser  extends AbstractSingleBeanDefinitionParser {
+}
